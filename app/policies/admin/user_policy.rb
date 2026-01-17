c52398014125c73aa48eefcf5c16aa8b363918d2
Sulav Aryal <aryalpurna6@gmail.com>
2026-01-17 13:39:54 +0545

    updated admin panel

--- app/policies/admin/user_policy.rb
+++ app/policies/admin/user_policy.rb
@@ -1,15 +1,31 @@
 module Admin
   class UserPolicy < ApplicationPolicy
-    def index?; user_is_admin?; end
-    def show?; user_is_admin?; end
-    def create?; user_is_admin?; end
-    def update?; user_is_admin?; end
-    def destroy?; user_is_admin?; end
+    def index?
+      user.admin?
+    end
 
-    class Scope < Struct.new(:user, :scope)
-      def resolve
-        user&.admin? ? scope.all : scope.none
-      end
+    def show?
+      user.admin?
+    end
+
+    def new?
+      user.admin?
+    end
+
+    def create?
+      user.admin?
+    end
+
+    def edit?
+      user.admin?
+    end
+
+    def update?
+      user.admin?
+    end
+
+    def destroy?
+      user.admin?
     end
   end
 end
